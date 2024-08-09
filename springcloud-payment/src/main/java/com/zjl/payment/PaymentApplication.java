071ca3536696b5727ff8bdef254c1e0b9fd505dd
shiun006688 <1273816331@qq.com>
2024-08-09 23:46:28 +0800

    利用consul做配置中心和发现中心，实现了配置中心的更新和发现中心的注册功能

--- springcloud-payment/src/main/java/com/zjl/payment/PaymentApplication.java
+++ springcloud-payment/src/main/java/com/zjl/payment/PaymentApplication.java
@@ -2,10 +2,15 @@ package com.zjl.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(basePackages = {"com.zjl.cloud.mapper"})
+@RefreshScope
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class,args);
--- springcloud-payment/src/main/java/com/zjl/payment/controller/PaymentController.java
+++ springcloud-payment/src/main/java/com/zjl/payment/controller/PaymentController.java
@@ -3,6 +3,9 @@ package com.zjl.payment.controller;
 import com.zjl.cloud.domain.Pay;
 import com.zjl.payment.service.PaymentService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+
     @GetMapping("/hello/{id}")
     public Pay hello(@PathVariable("id")Integer id){
         return paymentService.getOne(id);
+    }
+    //这里遇到的一个配置刷新的坑，就是每次要去调用这个@value.可以观察到变化
+    @GetMapping("/config")
+    public String getConsulConfig(@Value("${com}") String consul_config){
+        return consul_config+System.currentTimeMillis();
     }
 
 }
